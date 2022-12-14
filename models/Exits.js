5669d2cff8ad32f5bedf16bcef62725756269adc
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-14 14:14:22 +0100

    exitschema done

---
+++ models/Exits.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type:[Number],
+        index: "2dsphere"
+    }
+});
+
+const exitSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    altitudeMSL: {
+        type: Number
+    },
+    hikeDuration: {
+        type: Number
+    },
+    exit: GeoSchema,
+    landing: GeoSchema,
+    parking: GeoSchema,
+})
